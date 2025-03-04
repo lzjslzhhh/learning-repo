c8cd332f9bd44ad48c65a8a4bdafc305f432e10b
lzjslzhhh <2734678150@qq.com>
2025-03-04 22:27:13 +0800

    3月4日学习日志提交

--- CODE_C++/test3.cpp
+++ CODE_C++/test3.cpp
@@ -1,22 +1,53 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
 using namespace std;
-int main(){
-    string s="a   god   example";
-    int i=0,j;
-    // while(cin>>s){
-        for(j=0;j<s.size();j++){
-            if(s[j]!=' '){
-                if(i!=0){
-                    s[i++]=' ';
+
+class Solution {
+    public:
+        bool repeatedSubstringPattern(string s) {
+            string temp=s+s;
+            temp.erase(temp.begin());
+            temp.erase(temp.end()-1);
+            return kmp(temp,s);
+        }
+        bool kmp(const string a,const string b){
+            vector<int> next(b.size());
+            getNext(&next[0],b);
+            int j=-1;
+            for(int i=1;i<a.size();i++){
+                while(j>=0&&a[i]!=b[j+1]){
+                    j=next[j];
                 }
-                while(s[j]!=' '&&j<s.size()){
-                    s[i++]=s[j++];
+                if(a[i]==b[j+1]){
+                    j++;
+                }
+                if(j==b.size()-1){
+                    return true;
                 }
             }
+            return false;
         }
-    // }
-    s.resize(i);
-    cout<<s<<i;
+        void getNext(int *next,const string s){
+            int j=-1;
+            next[0]=j;
+            for(int i=1;i<s.size();i++){
+                while(j>=0&&s[i]!=s[j+1]){
+                    j=next[j];
+                }
+                if(s[i]==s[j+1]){
+                    j++;
+                }
+                next[i]=j;
+            }
+        }
+    };
+
+int main(){
+    Solution sol;
+    string s="bb";
+    cout<<sol.repeatedSubstringPattern(s)<<endl;
     return 0;
 }
